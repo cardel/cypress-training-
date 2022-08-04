bae0174a2a4a3193865257168026a3a7c37527d2
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2022-08-04 09:43:17 -0500

    12. Listas de elementos, filtros y elementos dentro de elementos (#12)
    * 7. Configurar Integracion Continua (CI)
    
    * 12. Listas de elementos, filtros y elementos dentro de elementos

--- cypress/page/products-list.page.ts
+++ cypress/page/products-list.page.ts
@@ -1,18 +1,24 @@
 class ProductsListPage {
   private cartButton: string;
   private checkoutButton: string;
+  private htmlallElments: string;
 
   constructor() {
       this.cartButton = ".button-container a[title='Add to cart']";
       this.checkoutButton = ".button-container a[title='Proceed to checkout']";
+      this.htmlallElments = ".product-container";
   }
 
-  public addToCart(): void {
-      cy.get(this.cartButton).click();
-  }
-
-  public checkout(): void {
+  public proceedToCheckout(): void {
       cy.get(this.checkoutButton).click();
+  }
+
+  public addTShirtToCart(product:string): void {
+    this.findProductByName(product).find(this.cartButton).click();
+  }
+
+  private findProductByName (product:string) {
+    return cy.get(this.htmlallElments).filter(`:contains(${product})`);
   }
 }
 
